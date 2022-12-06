9112e8fdf348a729aa283f5faae0c014fe66d413
Marco Moscato <marcomoscatto@gmail.com>
2022-12-06 20:45:29 -0300

    Requisito 5 em andamento

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('Testa se a página contém um heading `h2` com o texto `Encountered Pokémon`', () => {
+  renderWithRouter(<App />);
+  const heading = screen.getByRole('heading', { name: /encountered pokémon/i, level: 2 });
+  expect(heading).toBeInTheDocument();
+});
+
+test('Testa se é exibido o próximo Pokémon da lista quando o botão Próximo Pokémon é clicado', async () => {
+  renderWithRouter(<App />);
+
+  const nextButton = screen.getByRole('button', { name: 'Próximo Pokémon' });
+  expect(nextButton).toBeInTheDocument();
+
+  const firstPokemon = screen.getByText(/pikachu/i);
+  expect(firstPokemon).toBeInTheDocument();
+});
+
+// test.todo('', () => {});
