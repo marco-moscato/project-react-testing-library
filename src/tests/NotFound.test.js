16857e8497163c14c0f281ce37f1f3b983cb5c3e
Marco Moscato <marcomoscatto@gmail.com>
2022-12-06 18:13:53 -0300

    Requisito 4 concluído;

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('Testa se a página contém um heading `h2` com o texto `Page requested not found`', () => {
+// Acessar
+  const { history } = renderWithRouter(<App />);
+
+  // Agir
+  act(() => {
+    history.push('/wrongpage');
+  });
+
+  // Testar
+  const heading = screen.getByRole('heading', { name: /Page requested not found/i, level: 2 });
+  expect(heading).toBeInTheDocument();
+});
+
+test('Teste se a página mostra a imagem `https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif`', () => {
+// Acessar
+  const { history } = renderWithRouter(<App />);
+
+  act(() => { history.push('/wrongPage'); });
+
+  // Agir
+  // Testar
+  const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  const image = screen.getByRole('img');
+  expect(image).toBeInTheDocument();
+  expect(image.src).toBe(src);
+});
